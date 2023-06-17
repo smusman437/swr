a96f8963a747257779b60617af4ef5e8bf59a486
Muhammad Usman <50750473+smusman437@users.noreply.github.com>
2023-06-17 14:47:56 +0500

    setup project

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -1,10 +1,13 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
-import './index.css'
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App.jsx";
+import "./index.css";
+import { preload } from "swr";
+import { getTodos, todosUrlEndpoint as cacheKey } from "./api/todosApi";
 
-ReactDOM.createRoot(document.getElementById('root')).render(
+preload(cacheKey, getTodos);
+ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <App />
-  </React.StrictMode>,
-)
+  </React.StrictMode>
+);
